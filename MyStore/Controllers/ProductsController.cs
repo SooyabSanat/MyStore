1d2f42448cc67a6a55726c32d68253f4b6da9d5b
Ali <azatazaz1ta9@gmail.com>
2025-03-21 17:44:52 +0330

    ?

---
+++ MyStore/Controllers/ProductsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Domain.Models;
+using MyStore.Data;
+
+namespace MyStore.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly MyStoreContext _context;
+
+        public ProductsController(MyStoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            var myStoreContext = _context.Products.Include(p => p.Category);
+            return View(await myStoreContext.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
